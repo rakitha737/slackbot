6df87c3bd2d66f4b71dfb2b61302f7d6fc27a4d7
Rakhitha Basnayake <basnayrc@mail.uc.edu>
2018-11-24 22:00:01 -0500

    Minor changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,9 +11,9 @@ module.exports = {
   deploy: {
     production: {
       user: 'node',
-      host: '<YOUR SERVER IP>',
+      host: '18.224.1.150',
       ref: 'origin/master',
-      repo: 'https://github.com/<YOUR_REPO_URL>',
+      repo: 'https://github.com/rakitha737/slackbot.git',
       path: '/srv/production',
       'post-deploy':
         'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production',
